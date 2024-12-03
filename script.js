a021a288cbf35dcb9442d4b07ea804a5da8fccae
JuanPabloDamiano365 <judamiano@uade.edu.ar>
2024-12-02 23:36:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("formulario").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Gracias por tu consulta. Nos pondremos en contacto contigo pronto.");
+});
